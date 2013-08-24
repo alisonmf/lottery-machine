811a820f6e86d300ea1f3ef41fdf9d751bc2b2e8
Alison Foxall <alison.mf@gmail.com>
2013-08-24 00:40:00 -0400

    init

---
+++ random.php
@@ -0,0 +1,18 @@
+<?php 
+
+// Random Numbers Script
+// by Alison Foxall, about 2004
+// uploaded to github for entertainment purposes
+
+$n1 = rand (1, 53);
+$n2 = rand (1, 53);
+$n3 = rand (1, 53);
+$n4 = rand (1, 53);
+$n5 = rand (1, 53);
+$n6 = rand (1, 53);
+
+//print out the numbers
+print "Your Lucky Lotto Numbers today are: <br /><br />
+<pre>$n1   $n2   $n3   $n4   $n5   $n6</pre>";
+
+?>
