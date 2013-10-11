ef042b619083ba55d0c69f25a776b4ba54dcd286
spontaneouslypresent <mwild8@gmail.com>
2013-10-11 13:14:12 -0400

    gobbles
    meow

--- random.php
+++ random.php
@@ -1,18 +1,25 @@
-<?php 
+<?php
 
 // Random Numbers Script
 // by Alison Foxall, about 2004
 // uploaded to github for entertainment purposes
+// "i like money" -idiocracy
 
-$n1 = rand (1, 53);
-$n2 = rand (1, 53);
-$n3 = rand (1, 53);
-$n4 = rand (1, 53);
-$n5 = rand (1, 53);
-$n6 = rand (1, 53);
+$output = array();
+$range = 6;
+
+for($i=1; $i<=$range; $i++)
+{
+   $gobbly_number = rand (1, 53);
+
+   $output[] = "<li>$gobbly_number</li>";
+}
+
+$output = implode('',$output);
 
 //print out the numbers
-print "Your Lucky Lotto Numbers today are: <br /><br />
-<pre>$n1   $n2   $n3   $n4   $n5   $n6</pre>";
-
+echo "
+Your Lucky Lotto Numbers today are:
+<br /><br />
+<ul>$output</ul>";
 ?>
